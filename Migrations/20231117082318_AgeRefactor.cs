3586c32e91366aeea85d5636a8f74655b03ed649
Teddy <dudetotheleft@gmail.com>
2023-11-17 00:27:01 -0800

    Refactored string "Age" property to a DateTime "Birthdate" & GetAge() method

---
+++ Migrations/20231117082318_AgeRefactor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catopia.Migrations
+{
+    public partial class AgeRefactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdoptionDescription",
+                table: "Cats");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "Cats",
+                newName: "BriefDescription");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthdate",
+                table: "Cats",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthdate",
+                table: "Cats");
+
+            migrationBuilder.RenameColumn(
+                name: "BriefDescription",
+                table: "Cats",
+                newName: "Age");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AdoptionDescription",
+                table: "Cats",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
