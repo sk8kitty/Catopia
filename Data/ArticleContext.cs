f8d47f00cc28c30be7f3ed01058aae16642aab6b
Teddy <dudetotheleft@gmail.com>
2023-12-08 15:14:50 -0800

    Created ArticleContext (db context)

---
+++ Data/ArticleContext.cs
@@ -0,0 +1,15 @@
+﻿using Catopia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catopia.Data
+{
+    public class ArticleContext : DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options) 
+        { 
+
+        }
+
+        public DbSet<Article> Articles { get; set;}
+    }
+}
